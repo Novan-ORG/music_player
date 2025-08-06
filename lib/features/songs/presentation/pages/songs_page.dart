f5edd1a6d3fedde6c5209646a735a780e2116005
TalebRafiepour <taleb_r75@yahoo.com>
2025-08-06 19:05:19 +0330

    feat: implement songs items widget

--- lib/features/songs/presentation/pages/songs_page.dart
+++ lib/features/songs/presentation/pages/songs_page.dart
@@ -1,62 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:just_audio/just_audio.dart';
-import 'package:on_audio_query_pluse/on_audio_query.dart';
+import 'package:music_player/features/songs/presentation/widgets/song_item.dart';
 
 class SongsPage extends StatelessWidget {
   const SongsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final audioPlayer = AudioPlayer();
     return Scaffold(
       appBar: AppBar(title: const Text('Songs Page')),
-      body: Center(
-        child: FutureBuilder<List<SongModel>>(
-          future: (() async {
-            final permissionStatus = await OnAudioQuery().permissionsRequest();
-            if (!permissionStatus) {
-              throw Exception('Permission denied');
-            } else {
-              final result = await OnAudioQuery().querySongs();
-              return result;
-            }
-          })(),
-          builder: (_, snapShot) {
-            if (snapShot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapShot.hasError) {
-              return Text('Error: ${snapShot.error}');
-            } else {
-              return ListView.builder(
-                itemCount: snapShot.data?.length ?? 0,
-                itemBuilder: (context, index) {
-                  final song = snapShot.data![index];
-                  return ListTile(
-                    title: Text(song.title),
-                    subtitle: Text(song.artist ?? 'Unknown Artist'),
-                    leading: QueryArtworkWidget(
-                      id: song.id,
-                      type: ArtworkType.AUDIO,
-                      artworkFit: BoxFit.fill,
-                    ),
-                    onTap: () async {
-                      try {
-                        await audioPlayer.setAudioSource(
-                          AudioSource.uri(Uri.parse(song.uri!)),
-                        );
-                        await audioPlayer.play();
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Error playing song: $e')),
-                        );
-                      }
-                    },
-                  );
-                },
-              );
-            }
-          },
-        ),
+      body: ListView.builder(
+        itemCount: 25,
+        itemBuilder: (_, index) {
+          return const SongItem();
+        },
       ),
     );
   }
---
+++ lib/features/songs/presentation/widgets/song_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class SongItem extends StatelessWidget {
+  const SongItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('Song Title', style: Theme.of(context).textTheme.titleMedium),
+      subtitle: Text(
+        'Artist Name',
+        style: Theme.of(context).textTheme.bodySmall,
+      ),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.asset(
+          'assets/images/song_cover.png',
+          fit: BoxFit.cover,
+          width: 50,
+          height: 50,
+          alignment: Alignment.center,
+        ),
+      ),
+      trailing: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        spacing: 12,
+        children: [
+          IconButton(
+            icon: Icon(Icons.favorite_border),
+            onPressed: () {
+              // Handle favorite action
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('favorite pressed')));
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {
+              // Handle more action
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text('more pressed')));
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
