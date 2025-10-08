c838f301889350f5d5332f30e54cc0c44cd8e545
TalebRafiepour <taleb_r75@yahoo.com>
2025-10-08 12:58:31 +0330

    feat: add bug reporting service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:luciq_flutter/luciq_flutter.dart';
 import 'package:music_player/app.dart';
 import 'package:music_player/injection/service_locator.dart';
 
@@ -8,5 +12,25 @@ Future<void> main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   setup();
   await getIt.allReady();
-  runApp(const MusicPlayerApp());
+  await dotenv.load();
+  final luciqToken = dotenv.get('LUCIQ_TOKEN');
+
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      Luciq.init(
+        token: luciqToken,
+        invocationEvents: [InvocationEvent.floatingButton],
+        debugLogsLevel: LogLevel.none,
+      );
+
+      FlutterError.onError = (FlutterErrorDetails details) {
+        Zone.current.handleUncaughtError(details.exception, details.stack!);
+      };
+
+      runApp(const MusicPlayerApp());
+    },
+    CrashReporting.reportCrash,
+  );
 }
