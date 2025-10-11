1e137d6ee703e4a24dd5f9d1caba02168c1bdc68
TalebRafiepour <taleb_r75@yahoo.com>
2025-10-12 00:09:47 +0330

    config: add the sentry as bug reporting service

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:luciq_flutter/luciq_flutter.dart';
 import 'package:music_player/app.dart';
 import 'package:music_player/injection/service_locator.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   await runZonedGuarded(
@@ -15,19 +15,28 @@ Future<void> main() async {
       setup();
       await getIt.allReady();
       await dotenv.load();
-      final luciqToken = dotenv.get('LUCIQ_TOKEN');
-      await Luciq.init(
-        token: luciqToken,
-        invocationEvents: [],
-        debugLogsLevel: LogLevel.none,
-      );
+      final sentryDSN = dotenv.get('SENTRY_DSN');
 
       FlutterError.onError = (FlutterErrorDetails details) {
         Zone.current.handleUncaughtError(details.exception, details.stack!);
       };
 
-      runApp(const MusicPlayerApp());
+      await SentryFlutter.init(
+        (options) {
+          options
+            ..dsn = sentryDSN
+            ..sendDefaultPii = true
+            ..enableLogs = true
+            ..tracesSampleRate = 1.0
+            ..profilesSampleRate = 1.0;
+          options.replay.sessionSampleRate = 0.1;
+          options.replay.onErrorSampleRate = 1.0;
+        },
+        appRunner: () => runApp(SentryWidget(child: const MusicPlayerApp())),
+      );
     },
-    CrashReporting.reportCrash,
+    (Object error, StackTrace stack) {
+      Sentry.captureException(error, stackTrace: stack);
+    },
   );
 }
