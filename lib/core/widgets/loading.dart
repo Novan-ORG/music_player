b5ed9475586c01b3e6b97954c1c532577d2e0235
ebrahimpourelham <ebrahimpourelham44@gmail.com>
2025-12-17 12:38:36 +0330

    refactor: loading widget

--- lib/core/widgets/loading.dart
+++ lib/core/widgets/loading.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatelessWidget {
   const Loading({super.key});
@@ -21,7 +20,7 @@ class EqualizerLoading extends StatefulWidget {
     this.barWidthFactor = 0.09,
     this.spacingFactor = 0.10,
     this.minHeightFactor = 0.12,
-    this.speed = const Duration(milliseconds: 1200),
+    this.speed = const Duration(milliseconds: 900),
   });
 
   final Color color;
@@ -38,7 +37,7 @@ class EqualizerLoading extends StatefulWidget {
 
 class _EqualizerLoadingState extends State<EqualizerLoading>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _c;
+  late final AnimationController _animationController;
 
   // frameA: the second bar has the greatest height
   static const frameA = [0.25, 1.00, 0.55, 0.40, 0.15];
@@ -51,118 +50,91 @@ class _EqualizerLoadingState extends State<EqualizerLoading>
   @override
   void initState() {
     super.initState();
-    _c = AnimationController(vsync: this, duration: widget.speed)..repeat();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: widget.speed,
+    )..repeat();
 
-    _bars = List.generate(widget.barCount, (i) {
-      final a = frameA[i.clamp(0, frameA.length - 1)];
-      final b = frameB[i.clamp(0, frameB.length - 1)];
+    _bars = List.generate(widget.barCount, _buildBarAnimation);
+  }
 
-      return TweenSequence<double>([
-        TweenSequenceItem(
-          tween: Tween(
-            begin: a,
-            end: b,
-          ).chain(CurveTween(curve: Curves.linear)),
-          weight: 50,
-        ),
-        TweenSequenceItem(
-          tween: Tween(
-            begin: b,
-            end: a,
-          ).chain(CurveTween(curve: Curves.linear)),
-          weight: 50,
-        ),
-      ]).animate(_c);
-    });
+  Animation<double> _buildBarAnimation(int index) {
+    final a = frameA[index.clamp(0, frameA.length - 1)];
+    final b = frameB[index.clamp(0, frameB.length - 1)];
+
+    return TweenSequence<double>([
+      TweenSequenceItem(
+        tween: Tween(
+          begin: a,
+          end: b,
+        ).chain(CurveTween(curve: Curves.linear)),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween(
+          begin: b,
+          end: a,
+        ).chain(CurveTween(curve: Curves.linear)),
+        weight: 50,
+      ),
+    ]).animate(_animationController);
   }
 
   @override
   void didUpdateWidget(covariant EqualizerLoading oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.speed != widget.speed) {
-      _c
+      _animationController
         ..duration = widget.speed
         ..repeat();
     }
   }
 
-  @override
-  void dispose() {
-    _c.dispose();
-    super.dispose();
+  List<Widget> _generateBars() {
+    final barWidth = widget.size * widget.barWidthFactor;
+    final spacing = widget.size * widget.spacingFactor;
+    final barHeightCal =
+        widget.size * (widget.minHeightFactor + (1 - widget.minHeightFactor));
+
+    return List.generate(widget.barCount, (index) {
+      return Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: spacing / 2,
+        ),
+        child: AnimatedBuilder(
+          animation: _bars[index],
+          builder: (context, _) {
+            final barHeight = barHeightCal * _bars[index].value;
+            return Container(
+              width: barWidth,
+              height: barHeight,
+              decoration: BoxDecoration(
+                color: widget.color,
+                borderRadius: BorderRadius.circular(
+                  barWidth / 2,
+                ),
+              ),
+            );
+          },
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final barWidth = widget.size * widget.barWidthFactor;
-    final spacing = widget.size * widget.spacingFactor;
-    final radius = barWidth / 2;
-
-    final minH = widget.size * widget.minHeightFactor;
-    final maxH = widget.size;
-
     return SizedBox(
       height: widget.size,
-      child: AnimatedBuilder(
-        animation: _c,
-        builder: (context, _) {
-          return Row(
-            mainAxisSize: MainAxisSize.min,
-            children: List.generate(widget.barCount, (i) {
-              final v = _bars[i].value; // 0..1
-              final h = minH + (maxH - minH) * v;
-
-              return Padding(
-                padding: EdgeInsets.symmetric(horizontal: spacing / 2),
-                child: SizedBox(
-                  width: barWidth,
-                  height: widget.size,
-                  child: Center(
-                    child: Container(
-                      width: barWidth,
-                      height: h,
-                      decoration: BoxDecoration(
-                        color: widget.color,
-                        borderRadius: BorderRadius.circular(radius),
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            }),
-          );
-        },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: _generateBars(),
       ),
     );
   }
-}
-
-class EqualizerLoadingWithPackage extends StatelessWidget {
-  const EqualizerLoadingWithPackage({
-    super.key,
-    this.color = Colors.white,
-    this.size = 80,
-    this.duration = const Duration(milliseconds: 1400),
-  });
-
-  final Color color;
-  final double size;
-  final Duration duration;
 
   @override
-  Widget build(BuildContext context) {
-    return SpinKitWave(
-      size: size,
-      type: SpinKitWaveType.center,
-      duration: duration,
-      itemBuilder: (context, index) {
-        return DecoratedBox(
-          decoration: BoxDecoration(
-            color: color,
-            borderRadius: BorderRadius.circular(30),
-          ),
-        );
-      },
-    );
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
